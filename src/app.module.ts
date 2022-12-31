a0196434ccc8a62b3a76aa0cd4f1b30eed9fe99d
Sebastian Trujillo <juanstt99@gmail.com>
2022-12-30 19:19:30 -0500

    add providers to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigProvider } from './providers/config.provider';
+import { DatabaseProvider } from './providers/database.provider';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigProvider.getConfigModule(),
+    DatabaseProvider.getDatabaseModule(),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigProvider],
 })
 export class AppModule {}
