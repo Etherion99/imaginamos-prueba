d19b4b33c815574aeb209acf30bac68a8e402e1b
Sebastian Trujillo <juanstt99@gmail.com>
2022-12-30 23:16:55 -0500

    add feature modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './modules/clients/clients.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
+import { MunicipalitiesModule } from './modules/municipalities/municipalities.module';
+import { TechniciansModule } from './modules/technicians/technicians.module';
+import { TicketsModule } from './modules/tickets/tickets.module';
 import { ConfigProvider } from './providers/config.provider';
 import { DatabaseProvider } from './providers/database.provider';
 
@@ -8,6 +13,11 @@ import { DatabaseProvider } from './providers/database.provider';
   imports: [
     ConfigProvider.getConfigModule(),
     DatabaseProvider.getDatabaseModule(),
+    ClientsModule,
+    DepartmentsModule,
+    MunicipalitiesModule,
+    TechniciansModule,
+    TicketsModule,
   ],
   controllers: [AppController],
   providers: [AppService, ConfigProvider],
