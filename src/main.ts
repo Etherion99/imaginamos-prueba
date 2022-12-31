834c093150cb58d5cadbabac11d9569c7a7f3696
Sebastian Trujillo <juanstt99@gmail.com>
2022-12-31 06:39:37 -0500

    add basic swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Imaginamos Prueba Técnica')
+    .setDescription(
+      'API para generar órdenes de servicio para el mantenimiento e instalación de soportes para TV',
+    )
+    .setVersion('1.0')
+    .build();
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(app, configSwagger),
+  );
 
   await app.listen(configService.get('app.port'));
 }
